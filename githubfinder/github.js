69d38d76d7ca32ed86fb1a264a988bbeb861ce68
Joshua Paul <veecthorpaul@gmail.com>
2021-10-12 18:26:22 +0100

    github finder

---
+++ githubfinder/github.js
@@ -0,0 +1,22 @@
+class Github {
+  constructor() {
+    this.client_id = 'de27cdc6d6bc75eb9b35';
+    this.client_secret = '654e0a0206a62ce97379f4cb96c2214d5fa52025';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
