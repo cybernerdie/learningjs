003d3b2591783aa9972704d32369f205cf4ad20d
Joshua Paul <veecthorpaul@gmail.com>
2021-10-12 10:04:02 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+// // PRIMITIVE
+
+// // String
+// const name = 'John Doe';
+// // Number
+// const age = 45;
+// // Boolean
+// const hasKids = true;
+// // Null
+// const car = null;
+// // Undefined
+// let test;
+// // Symbol
+// const sym = Symbol();
+
+// // REFERENCE TYPES - Objects
+// // Array
+// const hobbies = ['movies', 'music'];
+// // Object literal
+// const address = {
+//   city: 'Boston',
+//   state: 'MA'
+// }
+
+// address.person = 'Joshua';
+
+// console.log(address.person);
+
+// const numbers = [1,2,3,4,5];
+// // numbers.push(6);
+// console.log(typeof numbers);
+// // const today = new Date();
+// // console.log(today);
+// // console.log(typeof today);
+
+let val;
+
+val = document.all;
+
+console.log(val);
